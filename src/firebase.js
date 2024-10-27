78f7cec3313bdd6fe7a46aff75b2d29681392e81
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-10-27 15:02:47 +0530

    Toastify is Display message Properly

--- src/firebase.js
+++ src/firebase.js
@@ -41,6 +41,7 @@ const signup = async (name, email, password) => {
     } catch (error) {
         console.log(error);
         toast.error(error.code);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
@@ -49,7 +50,7 @@ const login = async (email, password) => {
         await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        toast.error(error.code);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
